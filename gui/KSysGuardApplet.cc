4c13f47ac029ab8d0be24743dd38b0662234fbb7
Dirk Mueller <mueller@kde.org>
2005-10-05 08:44:32 +0000

    SVN_SILENT: fix typo
    svn path=/trunk/KDE/kdebase/kdm/; revision=467416

--- gui/KSysGuardApplet.cc
+++ gui/KSysGuardApplet.cc
@@ -14,7 +14,7 @@
 
     You should have received a copy of the GNU General Public License
     along with this program; if not, write to the Free Software
-    Foundation, Inc., 51 Franklin Steet, Fifth Floor, Boston, MA  02110-1301, USA.
+    Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 
     KSysGuard is currently maintained by Chris Schlaeger
     <cs@kde.org>. Please do not commit any changes without consulting
