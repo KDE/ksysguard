538d783120295a10b72044ae63000c33e70839e7
John Tapsell <john.tapsell@kdemail.net>
2006-12-26 01:53:24 +0000

    Replace # in the tooltip with 0x25CF - i.e. a big dot.  Looks coooool
    svn path=/trunk/KDE/kdebase/workspace/; revision=616540

--- gui/SensorDisplayLib/FancyPlotter.cc
+++ gui/SensorDisplayLib/FancyPlotter.cc
@@ -279,13 +279,13 @@ void FancyPlotter::setTooltip()
     if(sensors().at( i)->isLocalhost()) {
       
       tooltip += QString( "%1%2%3 (%4)" ).arg( i != 0 ? "<br>" : "<qt>")
-            .arg("<font color=\"" + mPlotter->beamColors()[ i ].name() + "\">#</font>")
+            .arg("<font color=\"" + mPlotter->beamColors()[ i ].name() + "\">"+QChar(0x25CF)+"</font>")
             .arg( description )
 	    .arg( lastValue );
 
     } else {
       tooltip += QString( "%1%2%3:%4 (%5)" ).arg( i != 0 ? "<br>" : "<qt>" )
-                 .arg("<font color=\"" + mPlotter->beamColors()[ i ].name() + "\">#</font>")
+                 .arg("<font color=\"" + mPlotter->beamColors()[ i ].name() + "\">"+QChar(0x25CF)+"</font>")
                  .arg( sensors().at( i )->hostName() )
                  .arg( description )
 	         .arg( lastValue );
