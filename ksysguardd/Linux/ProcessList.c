a51179c9a97726f4266159be90b44d5c212ca335
John Tapsell <john.tapsell@kdemail.net>
2007-10-17 21:59:23 +0000

    Put __GNUC__ around #warning
    svn path=/trunk/KDE/kdebase/workspace/; revision=726517

--- ksysguardd/Linux/ProcessList.c
+++ ksysguardd/Linux/ProcessList.c
@@ -68,7 +68,9 @@ extern int sys_ioprio_get(int, int);
 #define __NR_ioprio_set         1274
 #define __NR_ioprio_get         1275
 #else
+#ifdef __GNUC__
 #warning "This architecture does not support IONICE.  Disabling ionice feature."
+#endif
 #undef HAVE_IONICE
 #endif
 /* Map these to SYS_ioprio_get */
