734083f69bfde74e60147b425138862bafa88086
İsmail Dönmez <ismail@kde.org>
2007-11-13 22:04:04 +0000

    SVN_SILENT Fix John's e-mail address
    svn path=/trunk/KDE/kdebase/workspace/; revision=736305

--- gui/ksysguard.cc
+++ gui/ksysguard.cc
@@ -1,7 +1,7 @@
 /*
     KSysGuard, the KDE System Guard
 
-    Copyright (c) 2006 - 2008 John Tapsell <john.tapsell@kdemail.org>
+    Copyright (c) 2006 - 2008 John Tapsell <john.tapsell@kde.org>
     Copyright (c) 1999 - 2001 Chris Schlaeger <cs@kde.org>
 
     This program is free software; you can redistribute it and/or
