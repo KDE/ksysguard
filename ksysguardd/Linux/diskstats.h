496e18f65c7085e361862455a3b86a48f8cf9faf
Tom Albers <toma@kde.org>
2007-11-24 21:33:41 +0000

    All the contributors to these files agree to a relicense from GPL2 to GPL2+ or the contrbutor had < 2 LOC.
    svn path=/trunk/KDE/kdebase/workspace/; revision=741124

--- ksysguardd/Linux/diskstats.h
+++ ksysguardd/Linux/diskstats.h
@@ -4,7 +4,7 @@
     Copyright (c) 2006 Greg Martyn <greg.martyn@gmail.com>
 
     This program is free software; you can redistribute it and/or
-    modify it under the terms of version 2 of the GNU General Public
+    modify it under the terms of version 2 or later of the GNU General Public
     License as published by the Free Software Foundation.
 
     This program is distributed in the hope that it will be useful,
--- ksysguardd/Linux/softraid.c
+++ ksysguardd/Linux/softraid.c
@@ -4,7 +4,7 @@
 	Copyright (c) 2007 Greg Martyn <greg.martyn@gmail.com>, John Tapsell <tapsell@kde.org>
 	
 	This program is free software; you can redistribute it and/or
-	modify it under the terms of version 2 of the GNU General Public
+	modify it under the terms of version 2 or later of the GNU General Public
 	License as published by the Free Software Foundation.
 	
 	This program is distributed in the hope that it will be useful,
