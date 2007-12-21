a79ed8aa5264939738b67925382bfe6b6246a5d4
Chusslove Illich <caslav.ilic@gmx.net>
2007-12-21 18:55:34 +0000

    Extract title attributes from .sgrd files.
    svn path=/trunk/KDE/kdebase/workspace/; revision=751361

--- gui/Messages.sh
+++ gui/Messages.sh
@@ -1,4 +1,5 @@
 #! /usr/bin/env bash
 $EXTRACTRC `find . -name "*.ui"` >> rc.cpp || exit 11
 $EXTRACTRC `find . -name "*.sgrd"` >> rc.cpp || exit 12
+$EXTRACTATTR --attr="WorkSheet,title" `find . -name "*.sgrd"` >> rc.cpp || exit 13
 $XGETTEXT `find . -name "*.cpp" -o -name "*.cc"` -o $podir/ksysguard.pot
