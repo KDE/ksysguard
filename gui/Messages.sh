53f62e3aa648cbc6364536dce39f757b8bb6f480
John Tapsell <john.tapsell@kdemail.net>
2007-12-22 11:44:24 +0000

    Extract the display titles as well
    svn path=/trunk/KDE/kdebase/workspace/; revision=751602

--- gui/Messages.sh
+++ gui/Messages.sh
@@ -1,4 +1,4 @@
 #! /usr/bin/env bash
 $EXTRACTRC `find . -name "*.ui"` >> rc.cpp || exit 11
-$EXTRACTATTR --attr="WorkSheet,title" `find . -name "*.sgrd"` >> rc.cpp || exit 13
+$EXTRACTATTR --attr="WorkSheet,title" --attr="display,title" `find . -name "*.sgrd"` >> rc.cpp || exit 13
 $XGETTEXT `find . -name "*.cpp" -o -name "*.cc"` -o $podir/ksysguard.pot
