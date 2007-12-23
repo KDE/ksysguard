ed41e8f26b6f5ed3f7c4e9d81087f10dfd2fe222
Tom Albers <toma@kde.org>
2007-12-23 15:54:38 +0000

    RELICENSE
    svn path=/trunk/KDE/kdebase/workspace/; revision=752108

--- gui/SensorDisplayLib/DummyDisplay.cc
+++ gui/SensorDisplayLib/DummyDisplay.cc
@@ -4,8 +4,9 @@
     Copyright (c) 1999, 2000, 2001 Chris Schlaeger <cs@kde.org>
 
     This program is free software; you can redistribute it and/or
-    modify it under the terms of version 2 of the GNU General Public
-    License as published by the Free Software Foundation.
+    modify it under the terms of the GNU General Public
+    License version 2 or at your option version 3 as published by
+    the Free Software Foundation.
 
     This program is distributed in the hope that it will be useful,
     but WITHOUT ANY WARRANTY; without even the implied warranty of
