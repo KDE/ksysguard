e6787ced870c0401b5159e0267763d3159d275b2
John Tapsell <john.tapsell@kdemail.net>
2008-01-02 17:38:00 +0000

    Support for lmsensors 3.  Thanks to Chetan Reddy for the patch, and help from Hans de Goede the lmsensors maintainer.
    svn path=/trunk/KDE/kdebase/workspace/; revision=756064

--- ksysguardd/Linux/lmsensors.c
+++ ksysguardd/Linux/lmsensors.c
@@ -38,7 +38,12 @@ typedef struct
 {
   char* fullName;
   const sensors_chip_name* scn;
+#if SENSORS_API_VERSION & 0x400
+  const sensors_feature *sf;
+  const sensors_subfeature *sfd;
+#else
   const sensors_feature_data* sfd;
+#endif
 } LMSENSOR;
 
 static CONTAINER LmSensors;
@@ -73,15 +78,80 @@ static LMSENSOR* findMatchingSensor( const char* name )
 
 static const char *chipName(const sensors_chip_name *chip) {
   static char buffer[256];
+#if SENSORS_API_VERSION & 0x400
+  sensors_snprintf_chip_name(buffer, sizeof(buffer), chip);
+#else /* SENSORS_API_VERSION & 0x400 */
   if (chip->bus == SENSORS_CHIP_NAME_BUS_ISA)
     sprintf (buffer, "%s-isa-%04x", chip->prefix, chip->addr);
   else if (chip->bus == SENSORS_CHIP_NAME_BUS_PCI)
     sprintf (buffer, "%s-pci-%04x", chip->prefix, chip->addr);
   else
     sprintf (buffer, "%s-i2c-%d-%02x", chip->prefix, chip->bus, chip->addr);
+#endif /* SENSORS_API_VERSION & 0x400 */
   return buffer;
 }
 
+#if SENSORS_API_VERSION & 0x400
+const char *getSensorValueType( const sensors_subfeature *sbf )
+{
+    switch (sbf->type) {
+    case SENSORS_SUBFEATURE_IN_BEEP:
+    case SENSORS_SUBFEATURE_FAN_BEEP:
+    case SENSORS_SUBFEATURE_TEMP_BEEP:
+    case SENSORS_SUBFEATURE_BEEP_ENABLE:        
+        return "bool";
+    default:
+        return "float";
+    }
+    return NULL;
+}
+
+void initLmSensors( struct SensorModul* sm )
+{
+  const sensors_chip_name* scn;
+  char buffer[BUFFER_SIZE_LMSEN];
+  int nr = 0;
+
+  if ( sensors_init( NULL ) ) {
+    LmSensorsOk = -1;
+    return;
+  }
+
+  LmSensors = new_ctnr();
+  while ( ( scn = sensors_get_detected_chips( NULL, &nr ) ) != NULL ) {
+    int nr1, nr2;
+    const sensors_feature* sf;
+    nr1 = 0;
+    while ( ( sf = sensors_get_features( scn, &nr1 ) ) != 0 ) {
+      nr2 = 0;
+      const sensors_subfeature *ssubf;
+      while ( ( ssubf = sensors_get_all_subfeatures( scn, sf, &nr2 ) ) != 0 ) {
+        if ( ssubf->flags & SENSORS_MODE_R /* readable feature */) {
+          LMSENSOR* p;
+
+          p = (LMSENSOR*)malloc( sizeof( LMSENSOR ) );
+
+          snprintf( buffer, BUFFER_SIZE_LMSEN, "lmsensors/%s/%s", chipName(scn), ssubf->name );
+
+          p->fullName = strndup(buffer, BUFFER_SIZE_LMSEN);
+
+          p->scn = scn;
+          p->sf = sf;
+          p->sfd = ssubf;
+          if ( search_ctnr( LmSensors, sensorCmp, p ) < 0 ) {
+            push_ctnr( LmSensors, p );
+            registerMonitor( p->fullName, getSensorValueType(ssubf), printLmSensor, printLmSensorInfo, sm );
+          } else {
+            free( p->fullName );
+            free( p );
+          }
+        }
+      }
+    }
+  }
+  bsort_ctnr( LmSensors, sensorCmp );
+}
+#else /* SENSORS_API_VERSION & 0x400 */
 void initLmSensors( struct SensorModul* sm )
 {
   const sensors_chip_name* scn;
@@ -140,7 +210,7 @@ void initLmSensors( struct SensorModul* sm )
   }
   bsort_ctnr( LmSensors, sensorCmp );
 }
-
+#endif /* SENSORS_API_VERSION & 0x400 */
 
 void exitLmSensors( void )
 {
@@ -156,8 +226,11 @@ void printLmSensor( const char* cmd )
     fprintf( CurrentClient, "0\n" );
     return;
   }
-
+#if SENSORS_API_VERSION & 0x400
+  sensors_get_value( s->scn, s->sfd->number, &value );
+#else
   sensors_get_feature( *(s->scn), s->sfd->number, &value );
+#endif
   fprintf( CurrentClient, "%f\n", value );
 }
 
@@ -172,7 +245,12 @@ void printLmSensorInfo( const char* cmd )
 
   /* TODO: print real name here */
   char *label;
+#if SENSORS_API_VERSION & 0x400
+  label = sensors_get_label( s->scn, s->sf );
+  if (label == NULL) {
+#else
   if(sensors_get_label( *s->scn, s->sfd->number, &label ) != 0) {  /*error*/
+#endif
     fprintf( CurrentClient, "0\n" );
     return;
   }
@@ -182,7 +260,9 @@ void printLmSensorInfo( const char* cmd )
     fprintf( CurrentClient, "%s\t0\t0\trpm\n", label );
   else
     fprintf( CurrentClient, "%s\t0\t0\tV\n", label );  /* For everything else, say it's in volts. */
-
+#if SENSORS_API_VERSION & 0x400
+  free(label);
+#endif
 }
 
 #else /* HAVE_LMSENSORS */
