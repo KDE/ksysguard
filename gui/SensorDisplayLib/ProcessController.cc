1d3a31d861bfdaa58272545ca7be26e434ed292e
Dirk Mueller <mueller@kde.org>
2008-01-03 12:38:45 +0000

    RELICENSE: GPLv2 -> GPLv23
    svn path=/trunk/KDE/kdebase/workspace/; revision=756576

--- gui/SensorDisplayLib/ProcessController.cc
+++ gui/SensorDisplayLib/ProcessController.cc
@@ -5,18 +5,17 @@
 	Copyright (c) 2006 John Tapsell <john.tapsell@kde.org>
 
     This program is free software; you can redistribute it and/or
-    modify it under the terms version 2 of of the GNU General Public
-    License as published by the Free Software Foundation.
+    modify it under the terms of the GNU General Public License as
+    published by the Free Software Foundation; version 2 of
+    the License, or (at your option) version 3.
 
     This program is distributed in the hope that it will be useful,
     but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
     GNU General Public License for more details.
 
     You should have received a copy of the GNU General Public License
-    along with this program; if not, write to the Free Software
-    Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
-
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 #include <QDomElement>
 #include <QTimer>
