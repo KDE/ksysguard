a8c0737d1276d607c09bd97998152317e5d0b8e9
Laurent Montel <montel@kde.org>
2008-07-02 07:04:51 +0000

    Remove temporary file
    svn path=/trunk/KDE/kdebase/workspace/; revision=827042

--- gui/Messages.sh
+++ gui/Messages.sh
@@ -2,3 +2,4 @@
 $EXTRACTRC `find . -name "*.ui"` >> rc.cpp || exit 11
 $EXTRACTATTR --attr="WorkSheet,title" --attr="display,title" `find . -name "*.sgrd"` >> rc.cpp || exit 13
 $XGETTEXT `find . -name "*.cpp" -o -name "*.cc"` -o $podir/ksysguard.pot
+rm -f rc.cpp
