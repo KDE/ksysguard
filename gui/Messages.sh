f567d368b17597fdc071154e59fa0a01949c941a
Albert Astals Cid <tsdgeos@terra.es>
2010-08-22 13:44:03 +0000

    fix Messages.sh after yesterday change in extract-messages.sh
    svn path=/trunk/KDE/kdebase/workspace/; revision=1166696

--- gui/Messages.sh
+++ gui/Messages.sh
@@ -1,5 +1,5 @@
 #! /usr/bin/env bash
-$EXTRACTRC `find . -name "*.ui"` >> rc.cpp || exit 11
+$EXTRACTRC `find . -name "*.ui" -o -name "*.rc"` >> rc.cpp || exit 11
 $EXTRACTATTR --attr="WorkSheet,title" --attr="display,title" `find . -name "*.sgrd"` >> rc.cpp || exit 13
 $XGETTEXT `find . -name "*.cpp" -o -name "*.cc"` -o $podir/ksysguard.pot
 rm -f rc.cpp
