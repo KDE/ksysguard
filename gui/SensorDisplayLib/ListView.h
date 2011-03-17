4e8b33cd80f50a6348095e7507b35c694364d253
David Naylor <naylor.b.david@gmail.com>
2011-03-17 08:44:03 +0000

    ListView - add support for "KB" column type, to indicate a memory column
    ReviewedBy: John Tapsell

--- gui/SensorDisplayLib/ListView.h
+++ gui/SensorDisplayLib/ListView.h
@@ -29,6 +29,41 @@
 class ListViewSettings;
 class QTreeView;
 
+class ListViewModel : public QStandardItemModel {
+public:
+	ListViewModel(QObject * parent = 0 ) : QStandardItemModel(parent)
+	{
+	}
+
+	ListViewModel(int rows, int columns, QObject * parent = 0) : QStandardItemModel(rows, columns, parent)
+	{
+	}
+
+	void addColumnAlignment( Qt::AlignmentFlag align )
+	{
+		mAlignment.append(align);
+	}
+
+	void clear()
+	{
+		QStandardItemModel::clear();
+		mAlignment.clear();
+	}
+
+	QVariant data(const QModelIndex &index, int role) const
+	{
+		int column = index.column();
+
+		if ( role == Qt::TextAlignmentRole && column >= 0 && column < mAlignment.size() )
+			return mAlignment[column];
+		else
+			return QStandardItemModel::data(index, role);
+	}
+
+private:
+	QList<Qt::AlignmentFlag> mAlignment;
+};
+
 class ListView : public KSGRD::SensorDisplay
 {
 	Q_OBJECT
@@ -61,9 +96,9 @@ public Q_SLOTS:
 
 private:
 
-	typedef enum { Text, Int, Float, Time, DiskStat } ColumnType;
+	typedef enum { Text, Int, Float, Time, DiskStat, KByte } ColumnType;
 
-	QStandardItemModel mModel;
+	ListViewModel mModel;
 	QTreeView *mView;
 	ListViewSettings* lvs;
 	QByteArray mHeaderSettings;
