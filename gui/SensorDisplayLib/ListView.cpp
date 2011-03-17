34d5f4a05ed062766780b70434fcd72ff13a131c
John Tapsell <johnflux@gmail.com>
2011-03-17 16:37:29 +0000

    ListView - white space changes

--- gui/SensorDisplayLib/ListView.cpp
+++ gui/SensorDisplayLib/ListView.cpp
@@ -39,236 +39,236 @@
 #include "ListViewSettings.h"
 
 ListView::ListView(QWidget* parent, const QString& title, SharedSettings *workSheetSettings)
-	: KSGRD::SensorDisplay(parent, title, workSheetSettings)
+    : KSGRD::SensorDisplay(parent, title, workSheetSettings)
 {
-	QVBoxLayout *layout = new QVBoxLayout(this);
-	mView = new QTreeView(this);
-//	QSortFilterProxyModel *proxyModel = new QSortFilterProxyModel(this);
-//	proxyModel->setSourceModel(&mModel);
-	mView->setModel(&mModel);
-	mModel.setSortRole(Qt::UserRole);
-	layout->addWidget(mView);
-	this->setLayout(layout);
-	
-	mView->setContextMenuPolicy( Qt::CustomContextMenu );
-	connect(mView, SIGNAL(customContextMenuRequested(const QPoint &)), SLOT(showContextMenu(const QPoint &)));
-	connect(mView, SIGNAL(customContextMenuRequested(const QPoint &)), SLOT(showContextMenu(const QPoint &)));
+    QVBoxLayout *layout = new QVBoxLayout(this);
+    mView = new QTreeView(this);
+//    QSortFilterProxyModel *proxyModel = new QSortFilterProxyModel(this);
+//    proxyModel->setSourceModel(&mModel);
+    mView->setModel(&mModel);
+    mModel.setSortRole(Qt::UserRole);
+    layout->addWidget(mView);
+    this->setLayout(layout);
 
-	mView->setAlternatingRowColors(true);
-	mView->header()->setMovable(true);
-	mView->setSelectionMode( QAbstractItemView::NoSelection );
-	mView->setUniformRowHeights(true);
-	mView->setRootIsDecorated(false);
-	mView->header()->setSortIndicatorShown(true);
-	mView->header()->setClickable(true);
-	mView->setSortingEnabled(true);
+    mView->setContextMenuPolicy( Qt::CustomContextMenu );
+    connect(mView, SIGNAL(customContextMenuRequested(const QPoint &)), SLOT(showContextMenu(const QPoint &)));
+    connect(mView, SIGNAL(customContextMenuRequested(const QPoint &)), SLOT(showContextMenu(const QPoint &)));
 
-/*	QPalette palette;
-	palette.setColor(backgroundRole(), KSGRD::Style->backgroundColor());
-	setPalette(palette);*/
+    mView->setAlternatingRowColors(true);
+    mView->header()->setMovable(true);
+    mView->setSelectionMode( QAbstractItemView::NoSelection );
+    mView->setUniformRowHeights(true);
+    mView->setRootIsDecorated(false);
+    mView->header()->setSortIndicatorShown(true);
+    mView->header()->setClickable(true);
+    mView->setSortingEnabled(true);
 
-	setMinimumSize(50, 25);
+/*    QPalette palette;
+    palette.setColor(backgroundRole(), KSGRD::Style->backgroundColor());
+    setPalette(palette);*/
 
-	setPlotterWidget(mView);
-	setSizePolicy(QSizePolicy::MinimumExpanding,QSizePolicy::MinimumExpanding);
-	mView->setSizePolicy(QSizePolicy::MinimumExpanding,QSizePolicy::MinimumExpanding);
+    setMinimumSize(50, 25);
+
+    setPlotterWidget(mView);
+    setSizePolicy(QSizePolicy::MinimumExpanding,QSizePolicy::MinimumExpanding);
+    mView->setSizePolicy(QSizePolicy::MinimumExpanding,QSizePolicy::MinimumExpanding);
 }
 
 bool
 ListView::addSensor(const QString& hostName, const QString& sensorName, const QString& sensorType, const QString& title)
 {
-	if (sensorType != "listview")
-		return false;
-	if(sensorName.isEmpty()) return false;
+    if (sensorType != "listview")
+        return false;
+    if(sensorName.isEmpty()) return false;
 
-	kDebug() << "addSensor and sensorName is " << sensorName;
-	registerSensor(new KSGRD::SensorProperties(hostName, sensorName, sensorType, title));
+    kDebug() << "addSensor and sensorName is " << sensorName;
+    registerSensor(new KSGRD::SensorProperties(hostName, sensorName, sensorType, title));
 
-	setTitle(title);
+    setTitle(title);
 
-	/* To differentiate between answers from value requests and info
-	 * requests we use 100 for info requests. */
-	sendRequest(hostName, sensorName + '?', 100);
-	sendRequest(hostName, sensorName, 19);
-	return (true);
+    /* To differentiate between answers from value requests and info
+     * requests we use 100 for info requests. */
+    sendRequest(hostName, sensorName + '?', 100);
+    sendRequest(hostName, sensorName, 19);
+    return (true);
 }
 
 void ListView::updateList()
 {
-	for(int i = 0; i < sensors().count(); i++)
-		sendRequest(sensors().at(i)->hostName(), sensors().at(i)->name(), 19);
+    for(int i = 0; i < sensors().count(); i++)
+        sendRequest(sensors().at(i)->hostName(), sensors().at(i)->name(), 19);
 }
 
 ListView::ColumnType ListView::convertColumnType(const QString &type) const
 {
-  if ( type == "d" || type == "D" )
-    return Int;
-  else if ( type == "f" || type == "F" )
-    return Float;
-  else if ( type == "t" )
-    return Time;
-  else if ( type == "M" )
-    return DiskStat;
-  else if ( type == "KB" )
-    return KByte;
-  else if ( type == "%" )
-    return Percentage;
-  else
-    return Text;
+    if ( type == "d" || type == "D" )
+        return Int;
+    else if ( type == "f" || type == "F" )
+        return Float;
+    else if ( type == "t" )
+        return Time;
+    else if ( type == "M" )
+        return DiskStat;
+    else if ( type == "KB" )
+        return KByte;
+    else if ( type == "%" )
+        return Percentage;
+    else
+        return Text;
 }
 
 void
 ListView::answerReceived(int id, const QList<QByteArray>& answer)
 {
-	/* We received something, so the sensor is probably ok. */
-	sensorError(id, false);
+    /* We received something, so the sensor is probably ok. */
+    sensorError(id, false);
 
-	switch (id)
-	{
-		case 100: {
-			/* We have received the answer to a '?' command that contains
-			 * the information about the table headers. */
-			if (answer.count() != 2)
-			{
-				kDebug(1215) << "wrong number of lines";
-				return;
-			}
-			KSGRD::SensorTokenizer headers(answer[0], '\t');
-			KSGRD::SensorTokenizer colTypes(answer[1], '\t');
+    switch (id)
+    {
+        case 100: {
+            /* We have received the answer to a '?' command that contains
+             * the information about the table headers. */
+            if (answer.count() != 2)
+            {
+                kDebug(1215) << "wrong number of lines";
+                return;
+            }
+            KSGRD::SensorTokenizer headers(answer[0], '\t');
+            KSGRD::SensorTokenizer colTypes(answer[1], '\t');
 
-			/* add the new columns */
-			mModel.clear();
-			QStringList translatedHeaders;
-			for (uint i = 0; i < headers.count(); i++) {
-				translatedHeaders.append( i18nc("heading from daemon", headers[i]) );
-			}
+            /* add the new columns */
+            mModel.clear();
+            QStringList translatedHeaders;
+            for (uint i = 0; i < headers.count(); i++) {
+                translatedHeaders.append( i18nc("heading from daemon", headers[i]) );
+            }
 
-			for(uint i =0 ; i < colTypes.count(); i++) {
-				ColumnType type = convertColumnType(colTypes[i]);
-				mColumnTypes.append(type);
-				if (type == Text || type == DiskStat)
-					mModel.addColumnAlignment(Qt::AlignLeft);
-				else
-					mModel.addColumnAlignment(Qt::AlignRight);
-			}
-			
-			mModel.setHorizontalHeaderLabels(translatedHeaders);
-			//If we have some header settings to restore, we can do so now
-			if(!mHeaderSettings.isEmpty()) {
-				mView->header()->restoreState(mHeaderSettings);
-				mModel.sort( mView->header()->sortIndicatorSection(), mView->header()->sortIndicatorOrder() );
-			}
-			break;
-		}
-		case 19: {
-			for (int i = 0; i < answer.count(); i++) {
-				KSGRD::SensorTokenizer records(answer[i], '\t');
-				for (uint j = 0; j < records.count(); j++) {
-					QStandardItem *item = new QStandardItem();
-					item->setEditable(false);
-					switch( mColumnTypes[j] ) {
-					  case Int:
-						item->setData(records[j].toInt(), Qt::UserRole);
-						item->setText(records[j]);
-						break;
-					  case Percentage:
-						item->setData(records[j].toInt(), Qt::UserRole);
-						item->setText(records[j] + "%");
-						break;
-					  case Float:
-						item->setData(records[j].toFloat(), Qt::DisplayRole);
-						item->setData(records[j].toFloat(), Qt::UserRole);
-						break;
-					  case Time:
-						item->setData(QTime::fromString(records[j]), Qt::DisplayRole);
-						item->setData(QTime::fromString(records[j]), Qt::UserRole);
-						break;
-					  case KByte:
-						item->setData(records[j].toInt(), Qt::UserRole);
-						item->setText(i18nc("units in kb", "%1 K", records[j].toInt()));
-						break;
-					  case DiskStat:
-					  case Text:
-					  default:
-						item->setText(records[j]);
-						item->setData(records[j], Qt::UserRole);
-					}
-					mModel.setItem(i, j, item);
-				}
-			}
-			mModel.setRowCount(answer.count());
+            for(uint i =0 ; i < colTypes.count(); i++) {
+                ColumnType type = convertColumnType(colTypes[i]);
+                mColumnTypes.append(type);
+                if (type == Text || type == DiskStat)
+                    mModel.addColumnAlignment(Qt::AlignLeft);
+                else
+                    mModel.addColumnAlignment(Qt::AlignRight);
+            }
+
+            mModel.setHorizontalHeaderLabels(translatedHeaders);
+            //If we have some header settings to restore, we can do so now
+            if(!mHeaderSettings.isEmpty()) {
+                mView->header()->restoreState(mHeaderSettings);
+                mModel.sort( mView->header()->sortIndicatorSection(), mView->header()->sortIndicatorOrder() );
+            }
+            break;
+        }
+        case 19: {
+            for (int i = 0; i < answer.count(); i++) {
+                KSGRD::SensorTokenizer records(answer[i], '\t');
+                for (uint j = 0; j < records.count(); j++) {
+                    QStandardItem *item = new QStandardItem();
+                    item->setEditable(false);
+                    switch( mColumnTypes[j] ) {
+                      case Int:
+                        item->setData(records[j].toInt(), Qt::UserRole);
+                        item->setText(records[j]);
+                        break;
+                      case Percentage:
+                        item->setData(records[j].toInt(), Qt::UserRole);
+                        item->setText(records[j] + "%");
+                        break;
+                      case Float:
+                        item->setData(records[j].toFloat(), Qt::DisplayRole);
+                        item->setData(records[j].toFloat(), Qt::UserRole);
+                        break;
+                      case Time:
+                        item->setData(QTime::fromString(records[j]), Qt::DisplayRole);
+                        item->setData(QTime::fromString(records[j]), Qt::UserRole);
+                        break;
+                      case KByte:
+                        item->setData(records[j].toInt(), Qt::UserRole);
+                        item->setText(i18nc("units in kb", "%1 K", records[j].toInt()));
+                        break;
+                      case DiskStat:
+                      case Text:
+                      default:
+                        item->setText(records[j]);
+                        item->setData(records[j], Qt::UserRole);
+                    }
+                    mModel.setItem(i, j, item);
+                }
+            }
+            mModel.setRowCount(answer.count());
             mModel.sort( mView->header()->sortIndicatorSection(), mView->header()->sortIndicatorOrder() );
-			break;
-		}
-	}
+            break;
+        }
+    }
 }
 
 bool
 ListView::restoreSettings(QDomElement& element)
 {
-	kDebug() << "restore settings";
-	addSensor(element.attribute("hostName"), element.attribute("sensorName"), (element.attribute("sensorType").isEmpty() ? "listview" : element.attribute("sensorType")), element.attribute("title"));
+    kDebug() << "restore settings";
+    addSensor(element.attribute("hostName"), element.attribute("sensorName"), (element.attribute("sensorType").isEmpty() ? "listview" : element.attribute("sensorType")), element.attribute("title"));
 
-	//At this stage, we don't have the heading information, so we cannot setup the headers yet.
-	//Save the info, the restore later.
-	mHeaderSettings = QByteArray::fromBase64(element.attribute("treeViewHeader").toLatin1());
+    //At this stage, we don't have the heading information, so we cannot setup the headers yet.
+    //Save the info, the restore later.
+    mHeaderSettings = QByteArray::fromBase64(element.attribute("treeViewHeader").toLatin1());
 
-/*	QPalette pal = monitor->palette();
-	pal.setColor(QPalette::Link, restoreColor(element, "gridColor",
+/*    QPalette pal = monitor->palette();
+    pal.setColor(QPalette::Link, restoreColor(element, "gridColor",
                                                   KSGRD::Style->firstForegroundColor()));
-	pal.setColor(QPalette::Text, restoreColor(element, "textColor",
+    pal.setColor(QPalette::Text, restoreColor(element, "textColor",
                                                   KSGRD::Style->secondForegroundColor()));
-	pal.setColor(QPalette::Base, restoreColor(element, "backgroundColor",
+    pal.setColor(QPalette::Base, restoreColor(element, "backgroundColor",
                                                   KSGRD::Style->backgroundColor()));
 
-	monitor->setPalette( pal );
+    monitor->setPalette( pal );
 */
-	SensorDisplay::restoreSettings(element);
+    SensorDisplay::restoreSettings(element);
 
-	return true;
+    return true;
 }
 
 bool
 ListView::saveSettings(QDomDocument& doc, QDomElement& element)
 {
-	kDebug() << "save settings";
-	if(!sensors().isEmpty()) {
-		element.setAttribute("hostName", sensors().at(0)->hostName());
-		element.setAttribute("sensorName", sensors().at(0)->name());
-		element.setAttribute("sensorType", sensors().at(0)->type());
+    kDebug() << "save settings";
+    if(!sensors().isEmpty()) {
+        element.setAttribute("hostName", sensors().at(0)->hostName());
+        element.setAttribute("sensorName", sensors().at(0)->name());
+        element.setAttribute("sensorType", sensors().at(0)->type());
 
-		kDebug() << "sensorName is " << sensors().at(0)->name();
+        kDebug() << "sensorName is " << sensors().at(0)->name();
 
-/*	QPalette pal = monitor->palette();
-	saveColor(element, "gridColor", pal.color(QPalette::Link));
-	saveColor(element, "textColor", pal.color(QPalette::Text));
-	saveColor(element, "backgroundColor", pal.color(QPalette::Base));
+/*    QPalette pal = monitor->palette();
+    saveColor(element, "gridColor", pal.color(QPalette::Link));
+    saveColor(element, "textColor", pal.color(QPalette::Text));
+    saveColor(element, "backgroundColor", pal.color(QPalette::Base));
 */
-	}
-	element.setAttribute("treeViewHeader", QString::fromLatin1(mView->header()->saveState().toBase64()));
+    }
+    element.setAttribute("treeViewHeader", QString::fromLatin1(mView->header()->saveState().toBase64()));
 
-	SensorDisplay::saveSettings(doc, element);
-	return true;
+    SensorDisplay::saveSettings(doc, element);
+    return true;
 }
 
 void
 ListView::configureSettings()
 {
-	lvs = new ListViewSettings(this, "ListViewSettings");
-	Q_CHECK_PTR(lvs);
-	connect(lvs, SIGNAL(applyClicked()), SLOT(applySettings()));
+    lvs = new ListViewSettings(this, "ListViewSettings");
+    Q_CHECK_PTR(lvs);
+    connect(lvs, SIGNAL(applyClicked()), SLOT(applySettings()));
 
-/*	QPalette pal = monitor->palette();
-	lvs->setGridColor(pal.color(QPalette::Link));
-	lvs->setTextColor(pal.color(QPalette::Text));
-	lvs->setBackgroundColor(pal.color(QPalette::Base));
-	lvs->setTitle(title());
+/*    QPalette pal = monitor->palette();
+    lvs->setGridColor(pal.color(QPalette::Link));
+    lvs->setTextColor(pal.color(QPalette::Text));
+    lvs->setBackgroundColor(pal.color(QPalette::Base));
+    lvs->setTitle(title());
 */
-	if (lvs->exec())
-		applySettings();
+    if (lvs->exec())
+        applySettings();
 
-	delete lvs;
-	lvs = 0;
+    delete lvs;
+    lvs = 0;
 }
 
 void
@@ -289,15 +289,15 @@ ListView::applySettings()
 
   monitor->setPalette( pal );
 */
-  setTitle(lvs->title());
+    setTitle(lvs->title());
 }
 
 void
 ListView::applyStyle()
 {
-/*	QPalette pal = monitor->palette();
-	pal.setColor(QPalette::Link, KSGRD::Style->firstForegroundColor());
-	pal.setColor(QPalette::Text, KSGRD::Style->secondForegroundColor());
-	pal.setColor(QPalette::Base, KSGRD::Style->backgroundColor());
-	monitor->setPalette( pal );*/
+/*    QPalette pal = monitor->palette();
+    pal.setColor(QPalette::Link, KSGRD::Style->firstForegroundColor());
+    pal.setColor(QPalette::Text, KSGRD::Style->secondForegroundColor());
+    pal.setColor(QPalette::Base, KSGRD::Style->backgroundColor());
+    monitor->setPalette( pal );*/
 }
